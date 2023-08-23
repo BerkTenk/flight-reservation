3ea79c78785315a06e297353927780b4a879857e
Berk Tenkoğlu <berk.tenkoglu@gmail.com>
2023-08-23 14:14:55 +0300

    created api.json and updated flightlist

--- flight-search-app/src/Components/FlightList.js
+++ flight-search-app/src/Components/FlightList.js
@@ -1,8 +1,21 @@
 import React from 'react'
 
-function FlightList() {
+function FlightList({flights, loading }) {
+    if(loading){
+        return <p>Loading...</p>
+    }
+    if(!flights || flights.length===0){
+        return <p>Uçuş bulunamadi.</p>
+    }
   return (
-    <div>FlightList</div>
+        <ul>
+            {flights.map((flight)=>(
+                <li key ={flight.id}>
+                    {flight.id}
+                </li>
+            )
+            )}
+        </ul>
   )
 }
 
