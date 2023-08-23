0efc9d773fc2be3de487a695795efd4eea19a1d0
Berk Tenkoğlu <berk.tenkoglu@gmail.com>
2023-08-23 12:57:25 +0300

    updated searchform

--- flight-search-app/src/Components/SearchForm.js
+++ flight-search-app/src/Components/SearchForm.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function SearchForm() {
-  return (
-    <div>SearchForm</div>
-  )
+    const [departureAirport, setDepartureAirport] = useState("");
+    const [arrivalAirport, setArrivalAirport] = useState("");
+    const [departureDate, setDepartureDate] = useState("");
+    const [arrivalDate, setArrivalDate] = useState("");
+    const [oneWay, setOneWay] = useState(true);
+
+
+    return (
+        <div>
+            <input type="text" placeholder="Departure Airport" value={departureAirport} onChange={(e) => setDepartureAirport(e.target.value)} />
+            <input type="text" placeholder="Arrival Airport" value={arrivalAirport} onChange={(e) => setArrivalAirport(e.target.value)} />
+            <input type="date" placeholder="Departure Date" value={departureDate} onChange={(e) => setDepartureDate(e.target.value)} />
+            {!oneWay && <input type="date" placeholder="Arrival Date" value={arrivalDate} onChange={(e) => setArrivalDate(e.target.value)} />}
+            <label>
+                Tek Yönlü Uçuş
+                <input type="checkbox" checked={oneWay} onChange={() => setOneWay(!oneWay)} />
+            </label>
+        </div>
+    )
 }
 
 export default SearchForm
