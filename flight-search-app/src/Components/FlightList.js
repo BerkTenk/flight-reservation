6a379fd734eaca1cac29b990744d32a6daf7efd9
Berk Tenkoğlu <berk.tenkoglu@gmail.com>
2023-08-25 16:58:39 +0300

    updated api and flightlist

--- flight-search-app/src/Components/FlightList.js
+++ flight-search-app/src/Components/FlightList.js
@@ -1,81 +1,116 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-function FlightList({filters}) {
+function FlightList({ filters }) {
     const [flights, setFlights] = useState([]);
     const [loading, setLoading] = useState(true);
     const [sort, setSort] = useState("");
 
-    useEffect(()=>{
+    useEffect(() => {
         axios("http://localhost:3000/GetFlights")
-        .then((res)=>{
-            let filteredFlights=res.data;
-            if(filters.departureAirport){
-                filteredFlights=filteredFlights.filter((flight)=>
-                    flight.departure.toLowerCase().includes(filters.departureAirport.toLowerCase())
-                )
-            };
-            if (filters.arrivalAirport) {
-                filteredFlights = filteredFlights.filter((flight) =>
-                    flight.arrival.toLowerCase().includes(filters.arrivalAirport.toLowerCase())
-                );
-            };
-            if (filters.departureDate) {
-                filteredFlights = filteredFlights.filter((flight) =>
-                    flight.departureDate.toLowerCase().includes(filters.departureDate.toLowerCase())
-                );
-            };
-            if (filters.arrivalDate) {
-                filteredFlights = filteredFlights.filter((flight) =>
-                    flight.arrivalDate.toLowerCase().includes(filters.arrivalDate.toLowerCase())
-                );
-            };
-            if(sort==="price"){
-                filteredFlights.sort((a,b)=>a.price- b.price);
-            } else if(sort==="name"){
-                filteredFlights.sort((a,b)=>a.name.localeCompare(b.name));
-            }
+            .then((res) => {
+                let filteredFlights = res.data;
+                if (filters.departureAirport) {
+                    filteredFlights = filteredFlights.filter((flight) =>
+                        flight.departure.toLowerCase().includes(filters.departureAirport.toLowerCase())
+                    )
+                };
+                if (filters.arrivalAirport) {
+                    filteredFlights = filteredFlights.filter((flight) =>
+                        flight.arrival.toLowerCase().includes(filters.arrivalAirport.toLowerCase())
+                    );
+                };
+                if (filters.departureDate) {
+                    filteredFlights = filteredFlights.filter((flight) =>
+                        flight.departureDate.toLowerCase().includes(filters.departureDate.toLowerCase())
+                    );
+                };
+                if (filters.arrivalDate) {
+                    filteredFlights = filteredFlights.filter((flight) =>
+                        flight.arrivalDate.toLowerCase().includes(filters.arrivalDate.toLowerCase())
+                    );
+                };
+                if (sort === "price") {
+                    filteredFlights.sort((a, b) => a.price - b.price);
+                } else if (sort === "name") {
+                    filteredFlights.sort((a, b) => a.name.localeCompare(b.name));
+                } else if (sort === "duration") {
+                    filteredFlights.sort((a, b) => a.duration.localeCompare(b.duration));
+                } else if (sort === "depTime") {
+                    filteredFlights.sort((a, b) => a.departureTime.localeCompare(b.departureTime));
+                } else if (sort === "arrTime") {
+                    filteredFlights.sort((a, b) => a.arrivalTime.localeCompare(b.arrivalTime));
+                }
 
-            setFlights(filteredFlights);
-        })
-        .catch((error)=>{
-            console.error("An error occurred during the API request:", error);
-        })
-        .finally(()=>{
-            setLoading(false);
-        })
+                setFlights(filteredFlights);
+            })
+            .catch((error) => {
+                console.error("An error occurred during the API request:", error);
+            })
+            .finally(() => {
+                setLoading(false);
+            })
     }, [filters, sort]);
 
-    if(loading){
+
+    const [expandedFlightId, setExpandedFlightId] = useState(null);
+
+    const toggleFlightDetails = (flightId) => {
+        if (expandedFlightId === flightId) {
+            setExpandedFlightId(null);
+        } else {
+            setExpandedFlightId(flightId);
+        }
+    };
+
+    if (loading) {
         return <p>Loading...</p>
     }
-    if(!flights || flights.length===0){
+    if (!flights || flights.length === 0) {
         return <p>Flight not found.</p>
     }
 
 
-  return (
-    <div>
-        <br/>
-        <div className='bar'>
-            <button onClick={() => setSort("price")}>Sort By Price</button>
-            <button onClick={() => setSort("name")}>Sort By Name</button>
+    return (
+        <div>
+            <br />
+            <div className='bar'>
+                <button onClick={() => setSort("price")}>Sort By Price</button>
+                <button onClick={() => setSort("name")}>Sort By Name</button>
+                <button onClick={() => setSort("duration")}>Sort By Duration</button>
+                <button onClick={() => setSort("depTime")}>Sort By Departure Time</button>
+                <button onClick={() => setSort("arrTime")}>Sort By Arrival Time</button>
+            </div>
+            <ul className='list'>
+                {flights.map((flight) => (
+                    <li key={flight.id}>
+                        Name:{flight.name}  <br />
+                        Departure:{flight.departure} <br />
+                        Arrival:{flight.arrival} <br />
+                        Departure Date: {flight.departureDate} <br />
+                        Arrival Date: {flight.arrivalDate} <br />
+                        Departure Time: {flight.departureTime} <br />
+                        Arrival Time: {flight.arrivalTime} <br />
+                        Duration : {flight.duration} <br />
+                        Price: {flight.price}₺ <br />
+                        <button onClick={() => toggleFlightDetails(flight.id)}>
+                            {expandedFlightId === flight.id ? 'Hide Details' : 'Show Details'}
+                        </button>
+                        {expandedFlightId === flight.id && (
+                            <div>
+                                Departure City / Country: {flight.departureCity} <br />
+                                Arrival City / Country: {flight.arrivalCity}  <br />
+                                Departure Airport: {flight.departureAirport} <br />
+                                Arrival Airport: {flight.arrivalAirport} <br />
+                            </div>
+                        )}
+                        <hr />
+
+                    </li>
+                )
+                )}
+            </ul>
         </div>
-        <ul className='list'>
-            {flights.map((flight)=>(
-                <li key ={flight.id}>
-                Name:{flight.name}  <br/>
-                Departure:{flight.departure} <br/>
-                Arrival:{flight.arrival} <br/>
-                Departure Date: {flight.departureDate} <br/>
-                Arrival Date: {flight.arrivalDate} <br/>
-                Price: {flight.price}₺ <hr/>
-                
-                </li>
-            )
-            )}
-        </ul>
-        </div>
-  )
+    )
 }
 
 export default FlightList
