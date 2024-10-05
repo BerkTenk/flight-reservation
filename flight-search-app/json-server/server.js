a8c2678db2449605c8d13c5db91a7327d18d6f2f
Berk Tenkoğlu <berk.tenkoglu@gmail.com>
2024-10-06 00:32:04 +0300

    ENV LOVAL UPDATE

--- flight-search-app/json-server/server.js
+++ flight-search-app/json-server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 
+
 const app = express();
 app.use(cors()); // CORS'u etkinleştir
 
@@ -11,8 +12,8 @@ app.get('/api/flights', async (req, res) => {
         const url = 'https://api.schiphol.nl/public-flights/flights';
         const headers = {
             'Accept': 'application/json',
-            'app_id': '40ff444e',
-            'app_key': '33d69f3ac989c680c3180c4b929c5d1b',
+            'app_id': `${process.env.YOUR_APP_ID}`,
+            'app_key': `${process.env.YOUR_APP_KEY}`,
             'ResourceVersion': 'v4'
         };
 
